f9f93a007f0b29727f6b04bc279f6bb9d7415b77
tra600 <48340623+tra600@users.noreply.github.com>
2019-04-12 10:02:16 +0800

    Add files via upload

---
+++ 3A713238.14.cpp
@@ -0,0 +1,39 @@
+#include <iostream>  
+
+
+
+int main()
+
+{
+
+    int Sidelength1,Sidelength2,Sidelength3; 
+
+    printf("三根木條能不能拼成一個三角形。\n");
+
+	printf("定義：三角形的三邊長一定滿足任兩邊邊長和大於第三邊。\n");
+
+	printf("請輸入三邊長的長度：");
+
+scanf("%d%d%d",&Sidelength1,&Sidelength2,&Sidelength3);
+
+        if( Sidelength1+Sidelength2>Sidelength3 && Sidelength1+Sidelength3>Sidelength2 && Sidelength2+Sidelength3>Sidelength1 ) 
+
+        {
+
+        printf("結果：可以滿足\n");   
+
+        }
+
+        else
+
+        {
+
+        printf("結果：不可以滿足\n");   
+
+        }
+
+    system("pause"); 
+
+	return 0; 
+
+}
